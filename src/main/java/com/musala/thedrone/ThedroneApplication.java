c72acce6a015bbc66337b17e49295748de1ff583
xbolg47 <xbolg.47@gmail.com>
2023-02-25 14:56:03 +0100

    Initial spring boot project for the TheDrone service

---
+++ src/main/java/com/musala/thedrone/ThedroneApplication.java
@@ -0,0 +1,13 @@
+package com.musala.thedrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThedroneApplication.class, args);
+	}
+
+}
